49577c55befb56b2188ae57ce41d146af1ab0a1c
Ofer K <ofer.k@tikalk.com>
2016-04-20 00:38:00 +0300

    ux

--- wp-content/themes/bjj-child/home.php
+++ wp-content/themes/bjj-child/home.php
@@ -1,19 +1,23 @@
-<?php get_header ( 'archive' );
-?>
-<aside id="widget-area-top">
-    <?php if ( !function_exists ( 'dynamic_sidebar' ) || !dynamic_sidebar ( 'widget-area-top' ) ) {
-    } ?>
-</aside>
+<div class="site-title">
 
-<?php get_sidebar (); ?>
+    <b> Live </b>Bjj
+</div>
+<div class="site-desc">
+    <?php echo esc_html(get_bloginfo('description')) ?>
+</div>
+<div class='home'>
+
+    <?php
+    get_header('archive');
+?>
+
+</div>
+
 <div id="content" style="text-align: center" class="cf">
 
     <div class="post intro">
         <!--        <h1> Brazilian JiuJitsu Techniques</h1>-->
-        <p> Bjj Visual ! A collection of instructional videos arranged as a visual guide to Brazilian Jiu Jitsu
-            techniques by actual fight flow.<br>
-            No more technique namedroppings. Pick your preferred category and explore possible moves and strategies.</p>
-        <br>
+
         <?php
         ( $post->post_title ) ? $post->post_title : '';
         $next_moves = query_posts ( array( 'posts_per_page' => 20 ,
